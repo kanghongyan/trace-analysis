9195034ce28af178f1cd12cf90cb957e0d0f26bd
jiajianrong <jiajianrong@58.com>
2016-10-25 16:08:44 +0800

    scrsize done

--- public/javascripts/screenSize.js
+++ public/javascripts/screenSize.js
@@ -30,7 +30,8 @@ var screenSize = Vue.extend({
 				},
 				success: function(msg) {
 					if (msg.code == 1) {
-						that.showCountChart(msg);
+						that.showScrSizeChart(msg.data[0].data1);
+						that.showDprChart(msg.data[0].data2);
 					} else {
 						alert('查找失败');
 					}
@@ -42,146 +43,97 @@ var screenSize = Vue.extend({
 		},
 		
 		
-		showCountChart: function(initData) {
-			var screenChart = echarts.init(document.getElementById('screenChart-main')),
-				cakeChart = echarts.init(document.getElementById('screenCake-main')),
-				cakeData = [],
-				keyArray = [],
-				bubbleData = [];
-				lengendArray = [],
-				seriesArray = [],
-				index = 0,
-				colorArray = ['#FFCC00','#00FFCC','#663300','#FF6600','#663333','#CC6666','#FF6666',
-				              '#FF0000','#FFCC66','#FF9900','#FF9966','#CC3300','#996666','#660000','#FF3300'];
-			
-			for(var key in initData.data.screenSize){
-				var item = [],
-					it = [],
-					range,
-					lengendTitle;
-				if(key == '其他尺寸'){
-					continue;
-				}				
-				item = key.split('*');
-				lengendTitle = '[' + (parseInt(item[0]) - 15) + '-' + (parseInt(item[0]) + 15) + ']' + '*' + '[' + (parseInt(item[1]) - 25) + '-' + (parseInt(item[1]) + 25) + ']';
-				range = lengendTitle + ':' + initData['data']['screenSize'][key]['rate'];				
-				item.push(initData['data']['screenSize'][key]['num']);
-				item.push(range);
-				item.push(lengendTitle);
-				lengendArray.push(lengendTitle);
-				it.push(item);
-				bubbleData.push(it);
-				var colorIndex = index % colorArray.length,
-					seriesItem = {
-				        name: lengendArray[index],
-				        data: bubbleData[index],
-				        type: 'scatter',
-				        symbolSize: function (data) {
-				            return Math.sqrt(data[2])/5;
-				        },
-				        label: {
-				            emphasis: {
-				                show: true,
-				                formatter: function (param) {
-				                    return param.data[3];
-				                },
-				                position: 'top'
-				            }
-				        },
-				        itemStyle: {
-				            normal: {
-				                shadowBlur: 10,
-				                shadowColor: 'rgba(120, 36, 50, 0.5)',
-				                shadowOffsetY: 5,
-				                color: new echarts.graphic.RadialGradient(0.4, 0.3, 1, [{
-				                    offset: 0,
-				                    color: colorArray[colorIndex]
-				                }, {
-				                    offset: 1,
-				                    color: colorArray[colorIndex]
-				                }])
-				            }
-				        }
-				    };
-				seriesArray.push(seriesItem);
-				index ++;
-			}
-			screenOption = {
-			    backgroundColor: new echarts.graphic.RadialGradient(0.3, 0.3, 0.8, [{
-			        offset: 0,
-			        color: '#f7f8fa'
-			    }]),
-			    title: {
-			        text: '屏幕尺寸气泡图'
-			    },
-			    legend: {
-			        left: 135,
-			        data: lengendArray
-			    },
-			    xAxis: {
-			        splitLine: {
-			            lineStyle: {
-			                type: 'dashed'
-			            }
-			        }
-			    },
-			    yAxis: {
-			        splitLine: {
-			            lineStyle: {
-			                type: 'dashed'
-			            }
-			        },
-			        scale: true
-			    },
-			    series: seriesArray
-			};
-			if(initData.data.devicePixelRatio){
-				keyArray = Object.keys(initData.data.devicePixelRatio);
-			} 
-			for(var i = 0; i < keyArray.length; i++){
-				var perfix = keyArray[i] == "其他" ? '' : '设备像素比为';
-				cakeData.push({
-					value:initData['data']['devicePixelRatio'][keyArray[i]],
-					name: perfix + keyArray[i]
-				})
-				if(keyArray[i] != '其他'){
-					keyArray[i] = '设备像素比为' + keyArray[i];
-				}
-				
-			}
-			var cakeOption = {
-				    title : {
-				        text: '设备像素比',
-				        x:'center'
-				    },
-				    tooltip : {
-				        trigger: 'item',
-				        formatter: "{a} <br/>{b} : {c} ({d}%)"
-				    },
-				    legend: {
-				        orient: 'vertical',
-				        left: 'left',
-				        data:keyArray
-				    },
-				    series : [
-				        {
-				            name: '设备像素比值',
-				            type: 'pie',
-				            radius : '55%',
-				            center: ['50%', '60%'],
-				            data:cakeData,
-				            itemStyle: {
-				                emphasis: {
-				                    shadowBlur: 10,
-				                    shadowOffsetX: 0,
-				                    shadowColor: 'rgba(0, 0, 0, 0.5)'
-				                }
-				            }
-				        }
-				    ]
-				};
-			screenChart.setOption(screenOption);
-			cakeChart.setOption(cakeOption);
-		}
+		showScrSizeChart: function(data) {
+		    var screenChart = echarts.init(document.getElementById('screenChart-main')),
+                dataArray = Object.keys(data),
+                
+                legendArray = dataArray.map(function(item){
+                    var sa = item.split(','),
+                        last = sa.length-1;
+                    return sa[last]
+                }),
+                
+                seriesArray = dataArray.map(function(k){
+                    
+                    var dataArr = k.split(',');
+                    dataArr[0] = +dataArr[0];
+                    dataArr[1] = +dataArr[1];
+                    dataArr.splice(2,0,data[k]);
+                    dataArr[2] = Math.floor( dataArr[2]*10000 );
+                    dataArr = [dataArr];
+                    
+                    return {
+                        name: dataArr[0][4],
+                        data: dataArr,
+                        type: 'scatter',
+                        symbolSize: function (data) {
+                            return Math.sqrt(data[2]);
+                        },
+                        label: {
+                            emphasis: {
+                                show: true,
+                                formatter: function (param) {
+                                    return param.data[3];
+                                },
+                                position: 'top'
+                            }
+                        }
+                    }
+                })
+            
+            
+            var option = {
+                title: {text: ''},
+                legend: {right: 10, data: legendArray},
+                xAxis: {splitLine: {lineStyle: {type: 'dashed'}}},
+                yAxis: {splitLine: {lineStyle: {type: 'dashed'}}, scale: true},
+                series: seriesArray
+            };
+            
+            
+            screenChart.setOption(option);
+		},
+		
+        
+        
+        showDprChart: function(data) {
+            var cakeChart = echarts.init(document.getElementById('screenCake-main')),
+                keyArray = Object.keys(data),
+                cakeData = keyArray.map(function(k){
+                    return { name: k, value: data[k] }
+                });
+            
+            var cakeOption = {
+                    title : {
+                        text: 'DPR',
+                        x:'center'
+                    },
+                    tooltip : {
+                        trigger: 'item',
+                        formatter: "{a} <br/>{b} : {c} ({d}%)"
+                    },
+                    legend: {
+                        orient: 'vertical',
+                        left: 'left',
+                        data: keyArray
+                    },
+                    series : [{
+                        name: 'DPR',
+                        type: 'pie',
+                        radius : '55%',
+                        center: ['50%', '60%'],
+                        data: cakeData,
+                        itemStyle: {
+                            emphasis: {
+                                shadowBlur: 10,
+                                shadowOffsetX: 0,
+                                shadowColor: 'rgba(0, 0, 0, 0.5)'
+                            }
+                        }
+                    }]
+            };
+            
+            cakeChart.setOption(cakeOption);
+        }
 	}
 })
--- routes/api.js
+++ routes/api.js
@@ -15,6 +15,7 @@ var route_pageT = require('../backend/route_pageT');
 var route_performance = require('../backend/route_performance');
 var route_platform = require('../backend/route_platform');
 var route_browser = require('../backend/route_browser');
+var route_screenSize = require('../backend/route_screenSize');
 var route_hourPeak = require('../backend/route_hourPeak');
 var route_urlFilter = require('../backend/route_urlFilter');
 var route_specDaoliuPortal = require('../backend/route_specDaoliuPortal');
@@ -51,14 +52,17 @@ router.get('/totalV', route_pageV);
 /*基本信息统计--页面级PV UV LV统计*/
 router.get('/pageV', route_pageV);
 
+/*基本信息统计--页面级性能统计*/
+router.get('/performance', route_performance);
+
 /*基本信息统计--浏览器统计*/
 router.get('/browser', route_browser)
 
 /*基本信息统计--平台统计*/
 router.get('/platform', route_platform)
 
-/*基本信息统计--页面级性能统计*/
-router.get('/performance', route_performance);
+/*基本信息统计--屏幕大小统计*/
+router.get('/screenSize', route_screenSize);
 
 /*基本信息统计--时峰值统计*/
 router.get('/hourPeak', route_hourPeak);
@@ -68,160 +72,6 @@ router.get('/urlFilter', route_urlFilter);
 
 /*特殊信息统计--导流我的贷款portal*/
 router.get('/specDaoliuPortal', route_specDaoliuPortal);
-
-
-
-
-function getTimeByDate(dateStr) { //2016-08-09
-    var date = new Date(dateStr + ' 00:00');
-    var time = date.getTime();
-    return time;
-}
-function getTimePathByDate(date) {
-    var d = new Date(date);
-    var year = d.getFullYear();
-    var mouth = (parseInt(1 + d.getMonth()) + '').length > 1 ? parseInt(1 + d.getMonth()) : '0' + parseInt(1 + d.getMonth());
-    var day = (d.getDate() + '').length > 1 ? d.getDate() : '0' + d.getDate();
-    return {
-        year: year,
-        mouth: mouth,
-        day: day
-    };
-}
-function getParamer(listStr, paramer) {
-    var reg = new RegExp('(?:^|\|)' + paramer + '\=([^|]*)');
-    var name = listStr.match(reg);
-    if (!name || !name[1]) {
-        return '';
-    } else {
-        return name[1];
-    }
-}
-
-
-/*屏幕尺寸数据*/
-router.get('/screenSize', function(req, res, next) {
-    var project = req.query.project,
-        startTime = req.query.startTime,
-        endTime = req.query.endTime,
-
-        startTime_time = getTimeByDate(startTime),
-        endTime_time = getTimeByDate(endTime), 
-        result = {
-        devicePixelRatio:{},
-        screenSize:{}
-    };
-    if (fs.existsSync('infoData/' + project)) {
-        for (var i = startTime_time; i <= endTime_time; i += 1000 * 60 * 60 * 24) {
-            var o = getTimePathByDate(i),
-                year = o.year,
-                mouth = o.mouth,
-                day = o.day,
-                path = 'infoData/' + project + '/' + year + '-' + mouth + '-' + day + '.txt';
-            if (!fs.existsSync(path)) {
-                continue;
-            }
-            var data = fs.readFileSync(path, {'encoding': 'utf8'}),
-                dataMap = getScreenData(data);
-            for (var key in dataMap.devicePixelRatio){
-                result['devicePixelRatio'][key] = result['devicePixelRatio'][key] ? 
-                    result['devicePixelRatio'][key] + dataMap['devicePixelRatio'][key] : 
-                    dataMap['devicePixelRatio'][key];
-            }
-            for (var key in dataMap.screenSize){
-                if(!result['screenSize'][key]){
-                    result['screenSize'][key] ={};
-                }
-                if(result['screenSize'][key]['num']){
-                    result['screenSize'][key]['num'] += dataMap['screenSize'][key]['num'];
-                }else{
-                    result['screenSize'][key]['num'] = dataMap['screenSize'][key]['num'];
-                }
-            }
-            result.num = result.num ? result.num + dataMap.num : dataMap.num;
-        }
-    }
-    var num = result.num,
-        dealData = {
-           screenSize:{},
-           devicePixelRatio:{}
-        };    
-        for(var key in result.devicePixelRatio){
-            if(!dealData['devicePixelRatio'][key] && result['devicePixelRatio'][key]/num > 0.01){
-                dealData['devicePixelRatio'][key] = result['devicePixelRatio'][key];
-            }else if(dealData['devicePixelRatio'][key] && result['devicePixelRatio'][key]/num > 0.01){
-                dealData['devicePixelRatio'][key] += result['devicePixelRatio'][key];
-            }else if(!dealData['devicePixelRatio'][key] && result['devicePixelRatio'][key]/num < 0.01){
-                if(dealData['devicePixelRatio']['其他']){
-                    dealData['devicePixelRatio']['其他'] += result['devicePixelRatio'][key];
-                }else{
-                    dealData['devicePixelRatio']['其他'] = result['devicePixelRatio'][key];
-                }
-            }
-        }
-        dealData.num = num;
-        for(var key in result.screenSize){
-            if(!dealData['screenSize'][key] && result['screenSize'][key]['num']/num > 0.01){
-                dealData['screenSize'][key] = {};
-                dealData['screenSize'][key]['num'] = result['screenSize'][key]['num'];
-                dealData['screenSize'][key]['rate'] = parseFloat(dealData['screenSize'][key]['num'] / num).toFixed(2) * 100 + '%';
-            }else if(dealData['screenSize'][key] && result['screenSize'][key]['num']/num > 0.01){
-                dealData['screenSize'][key]['num'] += result['screenSize'][key]['num'];
-                dealData['screenSize'][key]['rate'] = parseFloat(dealData['screenSize'][key]['num'] / num).toFixed(2) * 100 + '%';
-            }
-        }       
-    result = dealData;
-    res.send({
-        code: 1,
-        data: result
-    });       
-})
-
-
-
-
-//基本信息－屏幕信息
-function getScreenData(data) {
-    if (!data) {
-        return;
-    }
-    var list = data.split('\n'),
-        result = {
-            devicePixelRatio:{},
-            screenSize:{}  
-        };
-    for (var i = 0; i < list.length - 1; i++) {
-        var scrsize = getParamer(list[i], 'scrsize'),
-            splitArray = [],
-            scrsizeArray = [];
-        if (!scrsize) {
-            continue;
-        }
-        result.num = result.num ? result.num + 1 : 1;
-        splitArray = scrsize.split('*');
-        scrsizeArray = splitArray.slice(0,2).reverse();
-        //scrsizeArray[0] = scrsizeArray[0] % 30 <= 15 ? parseInt(scrsizeArray[0]/30) * 30 : (parseInt(scrsizeArray[0]/30) + 1) * 30;
-        //scrsizeArray[1] = scrsizeArray[1]% 50 <= 25 ? parseInt(scrsizeArray[1]/50) * 50 : (parseInt(scrsizeArray[1]/50) + 1) * 50;
-        scrsizeArray[0] = parseInt(scrsizeArray[0]/30) * 30 + 15;
-        scrsizeArray[1] = parseInt(scrsizeArray[1]/50) * 50 + 25;
-        //console.log('[' + (scrsizeArray[0] - 15) + '-' + (scrsizeArray[0] + 15) + ']' + '*' + '[' + (scrsizeArray[1] - 25) + '-' + (scrsizeArray[1] + 25) + ']' );
-        result['devicePixelRatio'][splitArray[2]] = result['devicePixelRatio'][splitArray[2]] ? result['devicePixelRatio'][splitArray[2]] + 1 : 1;
-        //var range = '[' + (scrsizeArray[0] - 15) + '-' + (scrsizeArray[0] + 15) + ']' + '*' + '[' + (scrsizeArray[1] - 25) + '-' + (scrsizeArray[1] + 25) + ']';
-        if(!result['screenSize'][scrsizeArray[0] + '*' + scrsizeArray[1]]){
-            result['screenSize'][scrsizeArray[0] + '*' + scrsizeArray[1]] = {};
-        }
-        if(!result['screenSize'][scrsizeArray[0] + '*' + scrsizeArray[1]]['num']){
-            result['screenSize'][scrsizeArray[0] + '*' + scrsizeArray[1]]['num'] = 1;
-        }else{
-            result['screenSize'][scrsizeArray[0] + '*' + scrsizeArray[1]]['num'] += 1;
-        }
-        //result['screenSize'][scrsizeArray[0] + '*' + scrsizeArray[1]] = result['screenSize'][scrsizeArray[0] + 
-        //'*' + scrsizeArray[1]] ? result['screenSize'][scrsizeArray[0] + '*' + scrsizeArray[1]] + 1 : 1;
-    }
-    return result;
-}
-
-
 
 
 
