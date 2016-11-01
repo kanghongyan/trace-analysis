fe66acb438510ff548e2ef53166cc96031b611de
jiajianrong <jiajianrong@58.com>
2016-11-01 17:51:27 +0800

    route map

--- bin/child_computer.js
+++ bin/child_computer.js
@@ -3,63 +3,44 @@ var Promise = require("bluebird");
 var _ = require('lodash');
 var _util_fs_async = require('../backend/_util_fs_async');
 
-var compute_logic_browser = require('../backend/compute_logic_browser');
-var compute_logic_hourPeak = require('../backend/compute_logic_hourPeak');
-var compute_logic_pageList = require('../backend/compute_logic_pageList');
-var compute_logic_pageT = require('../backend/compute_logic_pageT');
-var compute_logic_pageV = require('../backend/compute_logic_pageV');
-var compute_logic_performance = require('../backend/compute_logic_performance');
-var compute_logic_platform = require('../backend/compute_logic_platform');
-var compute_logic_screenSize = require('../backend/compute_logic_screenSize');
-var compute_logic_urlFilter = require('../backend/compute_logic_urlFilter');
-var compute_logic_specDaoliuPortal = require('../backend/compute_logic_specDaoliuPortal');
+var computeMap = {
+    browser: require('../backend/compute_logic_browser'),
+    hourPeak: require('../backend/compute_logic_hourPeak'),
+    pageList: require('../backend/compute_logic_pageList'),
+    pageT: require('../backend/compute_logic_pageT'),
+    pageV: require('../backend/compute_logic_pageV'),
+    performance: require('../backend/compute_logic_performance'),
+    platform: require('../backend/compute_logic_platform'),
+    screenSize: require('../backend/compute_logic_screenSize'),
+    urlFilter: require('../backend/compute_logic_urlFilter'),
+    specDaoliuPortal: require('../backend/compute_logic_specDaoliuPortal')
+}
 
 
-process.on('message', function(d) {
+
+process.on('message', function(settings) {
     
-    console.log('child receive, ', arguments)
-    
-    var type = arguments[0].type
-    var category = arguments[0].category
-    var project = arguments[0].project
-    var startTime = arguments[0].startTime
-    var endTime = arguments[0].endTime
-    var page = arguments[0].page
-    var filter = arguments[0].filter
+    var type = settings.type
+    var category = settings.category
+    var project = settings.project
+    var startTime = settings.startTime
+    var endTime = settings.endTime
+    var page = settings.page
+    var filter = settings.filter
     
     
-    var compute_logic_callback;
+    var compute_logic_callback = computeMap[type];
     
-    if (type==='browser')
-        compute_logic_callback = compute_logic_browser;
     
-    if (type==='hourPeak')
-        compute_logic_callback = compute_logic_hourPeak;
+    if (type==='pageV' || type==='performance')
+        compute_logic_callback = compute_logic_callback(page);
     
-    if (type==='pageList')
-        compute_logic_callback = compute_logic_pageList;
-    
-    if (type==='pageT')
-        compute_logic_callback = compute_logic_pageT;
-    
-    if (type==='pageV')
-        compute_logic_callback = compute_logic_pageV(page);
-    
-    if (type==='performance')
-        compute_logic_callback = compute_logic_performance(page);
-    
-    if (type==='platform')
-        compute_logic_callback = compute_logic_platform;
-    
-    if (type==='screenSize')
-        compute_logic_callback = compute_logic_screenSize;
-    
-    if (type==='urlFilter')
-        compute_logic_callback = compute_logic_urlFilter(page, filter);
+    else if (type==='urlFilter')
+        compute_logic_callback = compute_logic_callback(page, filter);
     
     else if (type==='specDaoliuPortal') {
-        var analysis_callback_1 = compute_logic_specDaoliuPortal.analysis_callback_1;
-        var analysis_callback_2 = compute_logic_specDaoliuPortal.analysis_callback_2;
+        var analysis_callback_1 = compute_logic_callback.analysis_callback_1;
+        var analysis_callback_2 = compute_logic_callback.analysis_callback_2;
         
         var cb_1 = _util_fs_async( 'specData', project, startTime, endTime ).then(function(results){
                return analysis_callback_1(results);
