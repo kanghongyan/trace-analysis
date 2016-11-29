485aaa2429787b3a5b51d1c26053c39ecb8e40d9
jiajianrong <jiajianrong@58.com>
2016-11-29 19:37:40 +0800

    socket.io test

--- socket_test_app.js
+++ socket_test_app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app     = express();
 var server  = app.listen(80);
 var io = require('socket.io');
+var fs = require('fs');
 
 io = io.listen(server);
 
@@ -16,15 +17,73 @@ app.use(function(req, res, next) {
 
 
 
-io.on('connection', function(client) {  
-    console.log('Client connected...', client);
+io.on('connection', function(client) {
+    //console.log('Client connected...', client);
 
     client.on('join', function(data) {
-        console.log(data);
+        //console.log(data);
     });
     
     setInterval(()=>{
         client.emit('news', 'news from server');
     }, 10000)
     
+    
 });
+
+io.of('/upload', function(client){
+    console.log('upload')
+    logic(client);
+})
+
+
+
+function logic(socket) {
+    
+    
+
+    var writeStream = null;
+    var fileSize = 0;
+    var wrote = 0;
+    
+    socket.on('clientRequestFileTransfer', (fileInfo) => {
+        console.log(`Client request file transfer: ${fileInfo.name}(${fileInfo.size})`);
+    
+        fileSize = fileInfo.size;
+        wrote = 0;
+    
+        writeStream = fs.createWriteStream(__dirname + '/' + fileInfo.name);
+        writeStream.on('close', () => {
+            console.log('Write stream ended.');
+        });
+    
+        console.log('File created.');
+        socket.emit('serverGrantFileTransfer');
+    });
+    
+    socket.on('clientSentChunk', (chunk) => {
+        function write() {
+            var writeDone = writeStream.write(chunk);
+    
+            if(!writeDone) {
+                console.log('Back pressure!');
+                return writeStream.once('drain', write);
+            }
+            else {
+                wrote += chunk.length;
+                console.log(`Wrote chunks: ${chunk.length} / ${wrote} / ${fileSize}`);
+                socket.emit('serverRequestContinue');
+            }
+        }
+    
+        write();        
+    });
+    socket.on('clientFinishTransmission', () => {
+        writeStream.end();
+        console.log('Transmission complete!');
+    });
+    
+    
+    socket.emit('serverGrantFileTransfer', {});
+    
+}
