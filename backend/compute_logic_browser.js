6e29d83a68ab3dcd1ce03bdba1d9079b8672fcad
jiajianrong <jiajianrong@58.com>
2017-03-27 17:00:54 +0800

    more ua detective

--- backend/compute_logic_browser.js
+++ backend/compute_logic_browser.js
@@ -25,7 +25,16 @@ module.exports = function(results) {
                 return reArr[2]
             })
             .map(function(s){
-                return ['qianguiApp','58app','uc','qqbrowser','wx'].indexOf(s) == -1 ? 'others' : s
+                return ['qianguiApp',
+                        '58app',
+                        'uc',
+                        'qqbrowser',
+                        'wx',
+                        'sougoubrowser',
+                        'samsungbrowser',
+                        'miuibrowser',
+                        'baidubrowser',
+                        'baiduapp'].indexOf(s) == -1 ? 'others' : s
             })
             .countBy(function(s){
                 return s
